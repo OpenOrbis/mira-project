1116c5476082c8e3a97dfcee41085320f26d1193
ethylamine <46206558+ethylamine@users.noreply.github.com>
2020-05-08 03:59:39 +0000

    add eap hdd key offset

--- src/Utils/Kdlsym/Orbis455.hpp
+++ src/Utils/Kdlsym/Orbis455.hpp
@@ -170,4 +170,8 @@ for the platforms that do enable kernel ASLR (Address Space Layout Randomization
 
 // SceShellCore patches - use free prefix instead fake
 #define ssc_fake_to_free_patch                             0x0
+
+// Eap HDD Key
+#define kdlsym_addr_eap_hdd_key                             0x0258CCD0
+
 #endif
