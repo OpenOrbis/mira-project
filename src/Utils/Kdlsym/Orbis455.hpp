ade4d0bae726ac40380c1d84a8d7ee71ccfcfe27
Al Azif <33132478+Al-Azif@users.noreply.github.com>
2020-05-13 20:42:21 -0700

    Update Orbis455.hpp with SSU offsets

--- src/Utils/Kdlsym/Orbis455.hpp
+++ src/Utils/Kdlsym/Orbis455.hpp
@@ -210,7 +210,7 @@ for the platforms that do enable kernel ASLR (Address Space Layout Randomization
 #define ssc_fake_to_free_patch                             0x00D40F28
 
 // SceShellUI patches - debug patches
-#define ssu_sceSblRcMgrIsAllowDebugMenuForSettings_patch   0x0
-#define ssu_sceSblRcMgrIsStoreMode_patch                   0x0
+#define ssu_sceSblRcMgrIsAllowDebugMenuForSettings_patch   0x00019EA0
+#define ssu_sceSblRcMgrIsStoreMode_patch                   0x0001A1A0
 
 #endif
