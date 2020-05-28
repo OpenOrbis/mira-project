68d638a58c30df673ed14700fe8f5e89dc022b5e
Al Azif <33132478+Al-Azif@users.noreply.github.com>
2020-05-27 22:29:16 -0700

    Add missing PVS-Studio header

--- src/Plugins/BrowserActivator/BrowserActivator.cpp
+++ src/Plugins/BrowserActivator/BrowserActivator.cpp
@@ -1,3 +1,6 @@
+// This is an open source non-commercial project. Dear PVS-Studio, please check it.
+// PVS-Studio Static Code Analyzer for C, C++, C#, and Java: http://www.viva64.com
+
 #include "BrowserActivator.hpp"
 #include <Utils/Kdlsym.hpp>
 #include <Utils/Logger.hpp>
