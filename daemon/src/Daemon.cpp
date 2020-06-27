624e26b7a1c044839106f0f125b50ba03d36c59f
kuser <kuser@ubuntudev.lan>
2020-06-27 12:40:25 +0000

    Commit daemon project

---
+++ daemon/src/Daemon.cpp
@@ -0,0 +1,36 @@
+#include "Daemon.hpp"
+
+#include <memory>
+#include <cstdio>
+
+using namespace Mira;
+
+int main(void)
+{
+    // Create a new instance of our daemon
+    auto s_Daemon = std::make_unique<Daemon>();
+    if (s_Daemon == nullptr)
+    {
+        printf("err: could not initialize daemon.\n");
+        return -1;
+    }
+
+    // Try and load the daemon
+    if (!s_Daemon->OnLoad())
+    {
+        printf("err: could not load daemon.\n");
+        return -2;
+    }
+
+    // We never want to return from main
+    for (;;)
+        __asm__("nop");
+    
+    return 0;
+}
+
+bool Daemon::OnLoad()
+{
+
+    return true;
+}
