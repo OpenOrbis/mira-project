9525657a9455068adc50952b4d0d35f4dbbf7479
kuser <kuser@ubuntudev.lan>
2020-07-06 20:29:57 +0000

    Add 6.72 support

--- kernel/src/Utils/Kdlsym.hpp
+++ kernel/src/Utils/Kdlsym.hpp
@@ -10,35 +10,32 @@ extern "C" {
 #error MIRA_PLATFORM not set
 #endif
 
-#if defined(MIRA_UNSUPPORTED_PLATFORMS)
-    #if MIRA_PLATFORM==ONI_UNKNOWN_PLATFORM
-    #include "Kdlsym/Default.hpp"
-    #elif MIRA_PLATFORM==MIRA_PLATFORM_ORBIS_BSD_176
-    #include "Kdlsym/Orbis176.hpp"
-    #elif MIRA_PLATFORM==MIRA_PLATFORM_ORBIS_BSD_405
-    #include "Kdlsym/Orbis405.hpp"
-    #elif MIRA_PLATFORM==MIRA_PLATFORM_ORBIS_BSD_455
-    #include "Kdlsym/Orbis455.hpp"
-    #elif MIRA_PLATFORM==MIRA_PLATFORM_ORBIS_BSD_474
-    #include "Kdlsym/Orbis474.hpp"
-    #elif MIRA_PLATFORM==MIRA_PLATFORM_ORBIS_BSD_501
-    #include "Kdlsym/Orbis501.hpp"
-    #elif MIRA_PLATFORM==MIRA_PLATFORM_ORBIS_BSD_503
-    #include "Kdlsym/Orbis503.hpp"
-    #elif MIRA_PLATFORM==MIRA_PLATFORM_ORBIS_BSD_620
-    #include "Kdlsym/Orbis620.hpp"
-    #elif MIRA_PLATFORM==MIRA_PLATFORM_ORBIS_BSD_650
-    #include "Kdlsym/Orbis650.hpp"
-    #elif MIRA_PLATFORM==MIRA_PLATFORM_RASPI_ZERO
-    #include "Kdlsym/RaspberryPiZero.hpp"
-    #elif MIRA_PLATFORM==MIRA_PLATFORM_STEAM_LINK2
-    #include "Kdlsym/SteamLink2.hpp"
-    #endif
-#endif
-
-// Supported platforms, not gated behind MIRA_UNSUPPORTED_PLATFORMS
-#if MIRA_PLATFORM==MIRA_PLATFORM_ORBIS_BSD_505
+#if MIRA_PLATFORM==ONI_UNKNOWN_PLATFORM
+#include "Kdlsym/Default.hpp"
+#elif MIRA_PLATFORM==MIRA_PLATFORM_ORBIS_BSD_176
+#include "Kdlsym/Orbis176.hpp"
+#elif MIRA_PLATFORM==MIRA_PLATFORM_ORBIS_BSD_405
+#include "Kdlsym/Orbis405.hpp"
+#elif MIRA_PLATFORM==MIRA_PLATFORM_ORBIS_BSD_455
+#include "Kdlsym/Orbis455.hpp"
+#elif MIRA_PLATFORM==MIRA_PLATFORM_ORBIS_BSD_474
+#include "Kdlsym/Orbis474.hpp"
+#elif MIRA_PLATFORM==MIRA_PLATFORM_ORBIS_BSD_501
+#include "Kdlsym/Orbis501.hpp"
+#elif MIRA_PLATFORM==MIRA_PLATFORM_ORBIS_BSD_503
+#include "Kdlsym/Orbis503.hpp"
+#elif MIRA_PLATFORM==MIRA_PLATFORM_ORBIS_BSD_505
 #include "Kdlsym/Orbis505.hpp"
+#elif MIRA_PLATFORM==MIRA_PLATFORM_ORBIS_BSD_620
+#include "Kdlsym/Orbis620.hpp"
+#elif MIRA_PLATFORM==MIRA_PLATFORM_ORBIS_BSD_650
+#include "Kdlsym/Orbis650.hpp"
+#elif MIRA_PLATFORM==MIRA_PLATFORM_ORBIS_BSD_672
+#include "Kdlsym/Orbis672.hpp"
+#elif MIRA_PLATFORM==MIRA_PLATFORM_RASPI_ZERO
+#include "Kdlsym/RaspberryPiZero.hpp"
+#elif MIRA_PLATFORM==MIRA_PLATFORM_STEAM_LINK2
+#include "Kdlsym/SteamLink2.hpp"
 #endif
 
 // Kernel base address, this must be filled out on-startup (normally done in oni_initializeKernel)
