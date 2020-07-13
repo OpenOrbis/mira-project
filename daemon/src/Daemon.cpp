394c5498a1b4428b103e0db246390ecba6b15df6
kuser <kuser@ubuntudev.lan>
2020-07-13 16:55:07 +0000

    Add flatbuffers library for daemon

--- daemon/src/Daemon.cpp
+++ daemon/src/Daemon.cpp
@@ -1,11 +1,45 @@
 #include "Daemon.hpp"
-#include <mutex>
+
+#include <flatbuffers/flatbuffers.h>
+#include <External/flatbuffers/rpc_generated.h>
+
+#include <cstdio>
 
 using namespace Mira;
 
 Daemon::Daemon()
 {
+    flatbuffers::FlatBufferBuilder s_Builder;
+    Rpc::RpcHeaderBuilder s_HeaderBuilder(s_Builder);
+    s_HeaderBuilder.add_magic(Rpc::RpcMagics_Version2);
+    s_HeaderBuilder.add_category(Rpc::RpcCategory_SYSTEM);
+    auto s_Data = s_Builder.CreateVector((uint8_t*)nullptr, 0);
+    s_HeaderBuilder.add_data(s_Data);
 
+    auto s_Size = s_Builder.GetSize();
+    auto s_Buffer = s_Builder.GetBufferPointer();
+
+    const auto s_FileName = "daemon_test.bin";
+    FILE* s_File = fopen(s_FileName, "wb");
+    if (s_File == nullptr)
+    {
+        fprintf(stderr, "could not open file (%s) for writing (%d).\n", s_FileName, errno);
+        return;
+    }
+
+    auto s_Written = fwrite(s_Buffer, sizeof(uint8_t), s_Size, s_File);
+    if (s_Written <= 0)
+    {
+        fprintf(stderr, "could not write to file (%s) (%lu).", s_FileName, s_Written);
+        return;
+    }
+
+    auto s_Result = fclose(s_File);
+    if (s_Result < 0)
+    {
+        fprintf(stderr, "could not close file (%s) (%d).", s_FileName, s_Result);
+        return;
+    }
 }
 
 Daemon::~Daemon()
