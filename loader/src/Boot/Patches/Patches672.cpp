4595e24b2de12acee73cfa178a3dd39beecfe746
kuser <kuser@ubuntudev.lan>
2020-08-03 22:39:45 +0000

    Cleanup Loader Patches

--- loader/src/Boot/Patches/Patches672.cpp
+++ loader/src/Boot/Patches/Patches672.cpp
@@ -6,6 +6,7 @@
 void Mira::Boot::Patches::install_prerunPatches_672()
 {
 #if MIRA_PLATFORM == MIRA_PLATFORM_ORBIS_BSD_672
+	// NOTE: Only apply patches that the loader requires to run, the rest of them should go into Mira's ELF
 	// You must assign the kernel base pointer before anything is done
 	if (!gKernelBase)
 		return;
@@ -78,34 +79,6 @@ void Mira::Boot::Patches::install_prerunPatches_672()
 	kmem[1] = 0x90;
 	kmem[2] = 0x90;
 
-	// Enable MAP_SELF
-
-	// Patches: sceSblACMgrHasMmapSelfCapability
-	kmem = (uint8_t *)&gKernelBase[0x00233C40];
-	kmem[0] = 0xB8;
-	kmem[1] = 0x01;
-	kmem[2] = 0x00;
-	kmem[3] = 0x00;
-	kmem[4] = 0x00;
-	kmem[5] = 0xC3;
-
-	// Patches: sceSblACMgrIsAllowedToMmapSelf
-	kmem = (uint8_t *)&gKernelBase[0x00233C50];
-	kmem[0] = 0xB8;
-	kmem[1] = 0x01;
-	kmem[2] = 0x00;
-	kmem[3] = 0x00;
-	kmem[4] = 0x00;
-	kmem[5] = 0xC3;
-
-	// Patches: call    sceSblAuthMgrIsLoadable in vm_mmap2 (right above the only call to allowed to mmap self)
-	kmem = (uint8_t *)&gKernelBase[0x000AD2E4]; // xor eax, eax; nop; nop;
-	kmem[0] = 0x31;
-	kmem[1] = 0xC0;
-	kmem[2] = 0x90;
-	kmem[3] = 0x90;
-	kmem[4] = 0x90;
-
 	// Patch copyinstr
 	kmem = (uint8_t *)&gKernelBase[0x003C1CA3];
 	kmem[0] = 0x90;
@@ -119,74 +92,5 @@ void Mira::Boot::Patches::install_prerunPatches_672()
 	// Patch memcpy stack
 	kmem = (uint8_t *)&gKernelBase[0x003C15BD];
 	kmem[0] = 0xEB;
-
-	// ptrace patches
-	kmem = (uint8_t *)&gKernelBase[0x0010F892];
-	kmem[0] = 0x90;
-	kmem[1] = 0x90;
-	kmem[2] = 0x90;
-	kmem[3] = 0x90;
-	kmem[4] = 0x90;
-	kmem[5] = 0x90;
-
-	kmem = (uint8_t*)&gKernelBase[0x003C1857];
-	kmem[0] = 0x41;
-	kmem[1] = 0x41;
-
-	// disable delayed panics
-	kmem = (uint8_t*)&gKernelBase[0x0063C8CE];
-	kmem[0] = 0x90;
-	kmem[1] = 0x90;
-
-	// Disable this verif bullshit
-
-	// Disable sceSblSysVeriResume
-	kmem = (uint8_t*)&gKernelBase[0x0063CDB0];
-	kmem[0] = 0xC3;
-	kmem[1] = 0x90;
-	kmem[2] = 0x90;
-
-	// Disable sceSblSysVeriSuspend
-	kmem = (uint8_t*)&gKernelBase[0x0063CC90];
-	kmem[0] = 0xC3;
-	kmem[1] = 0x90;
-	kmem[2] = 0x90;
-
-	// Disable sysvericheckstatus_maybe
-	/*kmem = (uint8_t*)&gKernelBase[0x0063BD90];
-	kmem[0] = 0xC3;
-	kmem[1] = 0x90;
-	kmem[2] = 0x90;
-
-	// Disable sysvericheckstatus_maybe callout_ call
-	kmem = (uint8_t*)&gKernelBase[0x0063BDE4];
-	kmem[0] = 0x90;
-	kmem[1] = 0x90;
-	kmem[2] = 0x90;
-	kmem[3] = 0x90;
-	kmem[4] = 0x90;*/
-
-	// Disable delayedPanicCb
-	kmem = (uint8_t*)&gKernelBase[0x0063C8C0];
-	kmem[0] = 0xC3;
-	kmem[1] = 0x90;
-	kmem[2] = 0x90;
-
-	// Disable panic_fatal
-	/*kmem = (uint8_t*)&gKernelBase[0x0043BB0];
-	kmem[0] = 0xC3;
-	kmem[1] = 0x90;
-	kmem[2] = 0x90;*/
-
-	// Disable sub_FFFFFFFF8283C8D1
-	/*kmem = (uint8_t*)&gKernelBase[0x0063C8D1];
-	kmem[0] = 0xC3;
-	kmem[1] = 0x90;
-	kmem[2] = 0x90;
-
-	// NOP sled sceSblSysVeriThread
-	kmem = (uint8_t*)&gKernelBase[0x0063C950];
-	for (int i = 0; i < 0x327; ++i)
-		kmem[i] = 0x90; // NOP sled the entire shit, until the kthread_exit call*/
 #endif
 }
