a51d46edf40a35862a1edeb8be9e15db557977a5
Al Azif <33132478+Al-Azif@users.noreply.github.com>
2020-08-08 20:19:53 -0700

    Added missing patches from previous firmwares

--- kernel/src/Boot/Patches/Patches672.cpp
+++ kernel/src/Boot/Patches/Patches672.cpp
@@ -3,6 +3,10 @@
 
 #include <Boot/Patches.hpp>
 
+/*
+	Please, please, please!
+	Keep patches consistent with the used patch style for readability.
+*/
 void Mira::Boot::Patches::install_prerunPatches_672()
 {
 #if MIRA_PLATFORM==MIRA_PLATFORM_ORBIS_BSD_672
@@ -13,48 +17,49 @@ void Mira::Boot::Patches::install_prerunPatches_672()
 	// Use "kmem" for all patches
 	uint8_t *kmem;
 
-	// Patch dynlib_dlsym
-	kmem = (uint8_t*)&gKernelBase[0x1D895A];
-	kmem[0] = 0xE9;
-	kmem[1] = 0xC7;
-	kmem[2] = 0x01;
-	kmem[3] = 0x00;
-	kmem[4] = 0x00;
+	// Enable UART
+	kmem = (uint8_t *)&gKernelBase[0x01A6EB18];
+	kmem[0] = 0x00;
 
-	// Patch a function called by dynlib_dlsym
-	kmem = (uint8_t*)&gKernelBase[0x0041A2D0];
-	kmem[0] = 0x31; // xor eax, eax
-	kmem[1] = 0xC0;
-	kmem[2] = 0xC3;	// ret
-
-	// Patch sys_mmap
-	kmem = (uint8_t*)&gKernelBase[0x000AB57A];
-	kmem[0] = 0x37; // mov     [rbp+var_61], 33h ; '3'
-	kmem[3] = 0x37; // mov     sil, 33h ; '3'
-
-	// patch sys_setuid
-	kmem = (uint8_t*)&gKernelBase[0x0010BED0]; // call    priv_check_cred; overwrite with mov eax, 0
-	kmem[0] = 0xB8; // mov eax, 0
-	kmem[1] = 0x00;
-	kmem[2] = 0x00;
-	kmem[3] = 0x00;
-	kmem[4] = 0x00;
-
-	// patch sys_mprotect
-	kmem = (uint8_t*)&gKernelBase[0x00451DB8]; // jnz     loc_FFFFFFFF82652426; nop it out
+	// Verbose Panics
+	kmem = (uint8_t *)&gKernelBase[0x002ED33A];
 	kmem[0] = 0x90;
 	kmem[1] = 0x90;
 	kmem[2] = 0x90;
 	kmem[3] = 0x90;
 	kmem[4] = 0x90;
-	kmem[5] = 0x90;
 
-	// Enable rwx mapping in kmem_alloc
+	// sceSblACMgrIsAllowedSystemLevelDebugging
+	kmem = (uint8_t *)&gKernelBase[0x00233430];
+	kmem[0] = 0xB8;
+	kmem[1] = 0x01;
+	kmem[2] = 0x00;
+	kmem[3] = 0x00;
+	kmem[4] = 0x00;
+	kmem[5] = 0xC3;
+
+	kmem = (uint8_t *)&gKernelBase[0x00233BD0];
+	kmem[0] = 0xB8;
+	kmem[1] = 0x01;
+	kmem[2] = 0x00;
+	kmem[3] = 0x00;
+	kmem[4] = 0x00;
+	kmem[5] = 0xC3;
+
+	kmem = (uint8_t *)&gKernelBase[0x00233BF0];
+	kmem[0] = 0xB8;
+	kmem[1] = 0x01;
+	kmem[2] = 0x00;
+	kmem[3] = 0x00;
+	kmem[4] = 0x00;
+	kmem[5] = 0xC3;
+
+	// Enable rwx mapping
 	kmem = (uint8_t *)&gKernelBase[0x002507F5];
-	kmem[0] = 0x07; // set maxprot to RWX
+	kmem[0] = 0x07;
 
 	kmem = (uint8_t *)&gKernelBase[0x00250803];
-	kmem[0] = 0x07; // set maxprot to RWX
+	kmem[0] = 0x07;
 
 	// Patch copyin/copyout to allow userland + kernel addresses in both params
 	// copyin
@@ -78,8 +83,6 @@ void Mira::Boot::Patches::install_prerunPatches_672()
 	kmem[2] = 0x90;
 
 	// Enable MAP_SELF
-
-	// Patches: sceSblACMgrHasMmapSelfCapability
 	kmem = (uint8_t *)&gKernelBase[0x00233C40];
 	kmem[0] = 0xB8;
 	kmem[1] = 0x01;
@@ -88,7 +91,6 @@ void Mira::Boot::Patches::install_prerunPatches_672()
 	kmem[4] = 0x00;
 	kmem[5] = 0xC3;
 
-	// Patches: sceSblACMgrIsAllowedToMmapSelf
 	kmem = (uint8_t *)&gKernelBase[0x00233C50];
 	kmem[0] = 0xB8;
 	kmem[1] = 0x01;
@@ -97,8 +99,7 @@ void Mira::Boot::Patches::install_prerunPatches_672()
 	kmem[4] = 0x00;
 	kmem[5] = 0xC3;
 
-	// Patches: call    sceSblAuthMgrIsLoadable in vm_mmap2 (right above the only call to allowed to mmap self)
-	kmem = (uint8_t *)&gKernelBase[0x000AD2E4]; // xor eax, eax; nop; nop;
+	kmem = (uint8_t *)&gKernelBase[0x000AD2E4];
 	kmem[0] = 0x31;
 	kmem[1] = 0xC0;
 	kmem[2] = 0x90;
@@ -123,39 +124,140 @@ void Mira::Boot::Patches::install_prerunPatches_672()
 	kmem = (uint8_t *)&gKernelBase[0x0010F879];
 	kmem[0] = 0xEB;
 
-	// Enable debug rif's
-	kmem = (uint8_t*)&gKernelBase[0x66AEB0];
-	kmem[0] = 0xB0;
-	kmem[1] = 0x01;
-	kmem[2] = 0xC3;
-	kmem[3] = 0x90;
+	// second ptrace patch
+	// via DeathRGH
+	kmem = (uint8_t *)&gKernelBase[0x0010FD22];
+	kmem[0] = 0xE9;
+	kmem[1] = 0xE2;
+	kmem[2] = 0x02;
+	kmem[3] = 0x00;
+	kmem[4] = 0x00;
 
-	// Enable debug rifs 2
-	kmem = (uint8_t*)&gKernelBase[0x66AEE0];
-	kmem[0] = 0xB0;
-	kmem[1] = 0x01;
-	kmem[2] = 0xC3;
+	// setlogin patch (for autolaunch check)
+	kmem = (uint8_t *)&gKernelBase[0x0010EC1C];
+	kmem[0] = 0x48;
+	kmem[1] = 0x31;
+	kmem[2] = 0xC0;
 	kmem[3] = 0x90;
+	kmem[4] = 0x90;
 
-	// Disable pfs checks
-	kmem = (uint8_t*)&gKernelBase[0x6A8EB0];
+	// Patch to remove vm_fault: fault on nofault entry, addr %llx
+	kmem = (uint8_t *)&gKernelBase[0x000BC8F6];
+	kmem[0] = 0x90;
+	kmem[1] = 0x90;
+	kmem[2] = 0x90;
+	kmem[3] = 0x90;
+	kmem[4] = 0x90;
+	kmem[5] = 0x90;
+
+	// patch mprotect to allow RWX (mprotect) mapping 6.72
+	kmem = (uint8_t *)&gKernelBase[0x00451DB8];
+	kmem[0] = 0x90;
+	kmem[1] = 0x90;
+	kmem[2] = 0x90;
+	kmem[3] = 0x90;
+	kmem[4] = 0x90;
+	kmem[5] = 0x90;
+
+	// flatz disable pfs signature check
+	kmem = (uint8_t *)&gKernelBase[0x006A8EB0];
 	kmem[0] = 0x31;
 	kmem[1] = 0xC0;
 	kmem[2] = 0xC3;
-	kmem[3] = 0x90;
 
-	kmem = (uint8_t*)kdlsym(global_settings_base);
+	// flatz enable debug RIFs
+	kmem = (uint8_t *)&gKernelBase[0x0066AEB0];
+	kmem[0] = 0xB0;
+	kmem[1] = 0x01;
+	kmem[2] = 0xC3;
+
+	kmem = (uint8_t *)&gKernelBase[0x0066AEE0];
+	kmem[0] = 0xB0;
+	kmem[1] = 0x01;
+	kmem[2] = 0xC3;
+
+	// Enable *all* debugging logs (in vprintf)
+	// Patch by: SiSTRo
+	kmem = (uint8_t *)&gKernelBase[0x00123367];
+	kmem[0] = 0xEB;
+	kmem[1] = 0x3B;
+
+	// flatz allow mangled symbol in dynlib_do_dlsym
+	kmem = (uint8_t *)&gKernelBase[0x00417A27];
+	kmem[0] = 0x90;
+	kmem[1] = 0x90;
+	kmem[2] = 0x90;
+	kmem[3] = 0x90;
+	kmem[4] = 0x90;
+	kmem[5] = 0x90;
+
+	// Enable mount for unprivileged user
+	kmem = (uint8_t *)&gKernelBase[0x0044026A];
+	kmem[0] = 0x90;
+	kmem[1] = 0x90;
+	kmem[2] = 0x90;
+	kmem[3] = 0x90;
+	kmem[4] = 0x90;
+	kmem[5] = 0x90;
+
+	// patch suword_lwpid
+	// has a check to see if child_tid/parent_tid is in kernel memory, and it in so patch it
+	// Patch by: JOGolden
+	kmem = (uint8_t *)&gKernelBase[0x003C1AC2];
+	kmem[0] = 0x90;
+	kmem[1] = 0x90;
+
+	kmem = (uint8_t *)&gKernelBase[0x003C1AD1];
+	kmem[0] = 0x90;
+	kmem[1] = 0x90;
+
+	// Patch debug setting errors
+	kmem = (uint8_t *)&gKernelBase[0x00507B09];
+	kmem[0] = 0x00;
+	kmem[1] = 0x00;
+	kmem[2] = 0x00;
+	kmem[3] = 0x00;
+
+	kmem = (uint8_t *)&gKernelBase[0x00508D5C];
+	kmem[0] = 0x00;
+	kmem[1] = 0x00;
+	kmem[2] = 0x00;
+	kmem[3] = 0x00;
+
+  // Other patches KD added in 6.72, most of these have been in the exploit itself vs Mira ///////
+
+	// Patch dynlib_dlsym
+	kmem = (uint8_t *)&gKernelBase[0x1D895A];
+	kmem[0] = 0xE9;
+	kmem[1] = 0xC7;
+	kmem[2] = 0x01;
+	kmem[3] = 0x00;
+	kmem[4] = 0x00;
+
+	// Patch a function called by dynlib_dlsym
+	kmem = (uint8_t *)&gKernelBase[0x0041A2D0];
+	kmem[0] = 0x31; // xor eax, eax
+	kmem[1] = 0xC0;
+	kmem[2] = 0xC3;	// ret
+
+	// Patch sys_mmap
+	kmem = (uint8_t *)&gKernelBase[0x000AB57A];
+	kmem[0] = 0x37; // mov     [rbp+var_61], 33h ; '3'
+	kmem[3] = 0x37; // mov     sil, 33h ; '3'
+
+	// patch sys_setuid
+	kmem = (uint8_t *)&gKernelBase[0x0010BED0]; // call    priv_check_cred; overwrite with mov eax, 0
+	kmem[0] = 0xB8; // mov eax, 0
+	kmem[1] = 0x00;
+	kmem[2] = 0x00;
+	kmem[3] = 0x00;
+	kmem[4] = 0x00;
+
+	kmem = (uint8_t *)kdlsym(global_settings_base);
 	kmem[0x36] |= 0x14;
 	kmem[0x59] |= 0x01; // sceSblRcMgrIsAllowULDebugger
 	kmem[0x59] |= 0x02; // sceSblRcMgrIsAllowSLDebugger
 	kmem[0x5A] |= 0x01;
 	kmem[0x78] |= 0x01;
-
-	// Enable *all* debugging logs (in vprintf)
-	// Patch by: SiSTRo
-	kmem = (uint8_t*)&gKernelBase[0x00123367];
-	kmem[0] = 0xEB; // jmp +0x3D
-	kmem[1] = 0x3B;
-
 #endif
 }
