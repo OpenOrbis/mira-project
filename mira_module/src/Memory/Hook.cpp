515bc5e60085762c49fa987af84bdd5abf91b560
kuser <forums@allenthinks.com>
2021-03-20 12:54:38 +0000

    More work on hooks (UNTESTED)

--- mira_module/src/Memory/Hook.cpp
+++ mira_module/src/Memory/Hook.cpp
@@ -1,10 +1,14 @@
 #include "Hook.hpp"
+#include <Utils/Logger.hpp>
+
+#include <cstring>
 
 #define ARRAYSIZE(a) (sizeof(a) / sizeof(*(a)))
 
 extern "C"
 {
     #include <hde64/hde64.h>
+    #include <sys/mman.h>
 };
 
 using namespace Mira::Hooks;
@@ -60,8 +64,14 @@ const uint8_t c_Call32Relative[] =
     0x41, 0x41, 0x41, 0x41                          // # DW: RelativeAddress
 };
 
-void* Hook::CreateTrampoline(void* p_OriginalFunction, HookType p_HookType)
+void* Hook::AllocateNearAddress(void* p_NearAddress, uint32_t p_Size)
 {
+    // TODO: Get the VM maps from the Mira Driver
+    // Iterate through all of the vm_map's
+    // Find a free space close to the address we provided
+    // Allocate in that free space
+    // Check to see if it's > int32_t.MaxValue
+    // Return the allocation
     return nullptr;
 }
 
@@ -80,4 +90,125 @@ uint32_t Hook::GetTemplateHookSize(HookType p_HookType)
     default:
         return 0;
     }
+}
+
+bool Hook::ProtectMemory(void* p_Address, uint32_t p_Size, uint32_t p_Protection, uint32_t& p_OldProtection)
+{
+    // Validate arguments
+    if (p_Address == nullptr || p_Size == 0)
+        return false;
+
+    // TODO: Get the old protection
+
+    
+    // Change the protection of the memory
+    auto s_Ret = mprotect(p_Address, p_Size, p_Protection);
+    if (s_Ret == -1)
+    {
+        WriteLog(LL_Error, "could not mprotect (%p) with perms (%d), err: (%d).", p_Address, p_Protection, errno);
+        return false;
+    }
+
+    return true;
+}
+
+void* Hook::CreateTrampoline(void* p_OriginalFunction, HookType p_Type)
+{
+    // Validate original function
+    if (p_OriginalFunction == nullptr)
+        return nullptr;
+    
+    // Validate the hook type
+    if (p_Type <= HookType::None || 
+        p_Type >= HookType::COUNT ||
+        // We don't allow creating trampolines for calls
+        p_Type == HookType::CallRelative)
+    {
+        WriteLog(LL_Error, "invalid hook type.");
+        return nullptr;
+    }
+
+    // Calculate the needed backup size for hook + extra "correct" instructions
+    uint32_t s_BackupSize = GetMinimumHookSize(p_OriginalFunction, p_Type);
+    if (s_BackupSize == 0)
+    {
+        WriteLog(LL_Error, "there was an error getting min hook size.");
+        return nullptr;
+    }
+
+    // Validate that we have enough space to backup this function in hook
+    if (s_BackupSize > MaxBackupSize)
+    {
+        WriteLog(LL_Error, "(%d) > max backup size (%d).", s_BackupSize, MaxBackupSize);
+        return nullptr;
+    }
+
+    // Zero out the previous backup bytes
+    (void)memset(m_BackupBytes, 0, ARRAYSIZE(m_BackupBytes));
+
+    // Create a backup of the existing bytes
+    (void)memcpy(m_BackupBytes, p_OriginalFunction, s_BackupSize);
+
+    // Create a new trampoline in "memory space"
+    uint32_t s_TemplateSize = GetTemplateHookSize(HookType::JumpRip);
+    if (s_TemplateSize == 0)
+    {
+        WriteLog(LL_Error, "could not get the template size.");
+        return nullptr;
+    }
+
+    // Calculate the total trampoline size which are backup bytes + absolute jump to midfunction
+    uint32_t s_TrampolineSize = s_BackupSize + s_TemplateSize;
+
+    // Allocate new trampoline data
+    uint8_t* s_Trampoline = new uint8_t[s_TrampolineSize];
+    if (s_Trampoline == nullptr)
+    {
+        WriteLog(LL_Error, "could not allocate trampoline.");
+        return nullptr;
+    }
+    // Zero out trampoline
+    (void)memset(s_Trampoline, 0, s_BackupSize);
+
+    // Copy all bytes overwritten by the hook
+    (void)memcpy(s_Trampoline, m_BackupBytes, s_BackupSize);
+
+    // Create a new Jump-Rip template
+    uint8_t s_JumpRip[s_TemplateSize];
+    (void)memcpy(s_JumpRip, c_JumpRipTemplate, s_TemplateSize);
+    
+    // Set the absolute jump address
+    *(uint64_t*)(s_JumpRip + 6) = reinterpret_cast<uint64_t>(p_OriginalFunction) + s_BackupSize;
+
+    // Copy the absolute jump to the trampoline
+    (void)memcpy(s_Trampoline + s_BackupSize, s_JumpRip, ARRAYSIZE(s_JumpRip));
+
+    // Finally we need to change the protection on this memory
+    if (!ProtectMemory(s_Trampoline, s_TemplateSize, PROT_READ | PROT_EXEC))
+    {
+        WriteLog(LL_Error, "could not protect memory.");
+        delete [] s_Trampoline;
+        return nullptr;
+    }
+
+    // Return the completed trampoline
+    return s_Trampoline;
+}
+
+uint32_t Hook::GetMinimumHookSize(void* p_Target, HookType p_HookType)
+{
+    hde64s s_Dism;
+    uint32_t s_HookMinSize = GetTemplateHookSize(p_HookType);
+    uint32_t s_TotalLength = 0;
+
+    while (s_TotalLength < s_HookMinSize)
+    {
+        uint32_t s_InstructionLength = hde64_disasm(p_Target, &s_Dism);
+        if (s_Dism.flags & F_ERROR)
+            return 0;
+        
+        s_TotalLength += s_InstructionLength;
+    }
+
+    return s_TotalLength;
 }
