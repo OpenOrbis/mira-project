94576ff933f3058b78c9a8b2326b49a414586ca3
kiwidog <forums@allenthinks.com>
2022-01-02 03:30:42 -0800

    Update github workflow
    Convert daemon to prx for rapid testing

--- daemon/src/Entry.cpp
+++ daemon/src/Entry.cpp
@@ -2,6 +2,7 @@
 
 #include <memory>
 #include <cstdio>
+#include <thread>
 
 #include <Utils/Logger.hpp>
 
@@ -9,8 +10,39 @@
 #include <orbis/libkernel.h>
 #endif
 
+std::thread g_LaunchThread;
+
+extern "C" void trainer_load()
+{
+    g_LaunchThread = std::thread([]()
+    {
+        WriteLog(LL_Debug, "MiraDaemon Entry Point.");
+
+        // Create a new instance of our daemon
+        auto s_Daemon = Mira::Daemon::GetInstance();
+        if (s_Daemon == nullptr)
+        {
+            WriteLog(LL_Error, "could not create daemon.");
+            return -1;
+        }
+
+        // Try and load the daemon
+        if (!s_Daemon->OnLoad())
+        {
+            WriteLog(LL_Error, "could not load daemon.");
+            return -2;
+        }
+        
+        for (;;)
+            __asm__("nop");
+    });
+
+    g_LaunchThread.detach();
+}
+
+
 // Entry point
-int main(void)
+int daemon_entry(void)
 {
     WriteLog(LL_Debug, "MiraDaemon Entry Point.");
 
